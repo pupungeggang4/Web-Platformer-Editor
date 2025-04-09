79f019027dc78428911002230262ba6efa5fae2d
pupungeggang6 <pupungeggang6@gmail.com>
2025-04-09 14:09:14 +0900

    Start

---
+++ Game/Script/Asset.js
@@ -0,0 +1,13 @@
+let img = {
+    button: {
+        newFile: new Image(),
+        saveFile: new Image(),
+        loadFile: new Image(),
+    }
+}
+
+function imageLoad() {
+    img.icon.newFile.src = 'Image/ButtonNewFile.png'
+    img.icon.saveFile.src = 'Image/ButtonSaveFile.png'
+    img.icon.loadFile.src = 'Image/ButtonLoadFile.png'
+}
