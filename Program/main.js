3d01406869a9f3707d23939009241edaa37615bb
pupungeggang2 <kyongkyong2892@gmail.com>
2025-04-20 02:30:40 +0900

    Added Icon

--- Program/main.js
+++ Program/main.js
@@ -1,12 +1,14 @@
 const { app, BrowserWindow } = require('electron')
+var path = require('path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 1280,
-        height: 800
+        height: 800,
+        icon: path.join(__dirname, 'favicon.ico')
     })
 
-    win.loadFile('Program/index.html')
+    win.loadFile(path.join(__dirname, 'index.html'))
 }
 
 app.whenReady().then(() => {
