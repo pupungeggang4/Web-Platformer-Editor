c9d49b62ca3e13d224a71331302fdda50f2498cb
pupungeggang2 <kyongkyong2892@gmail.com>
2025-04-23 19:39:11 +0900

    Start

--- Program/Script/Editor.js
+++ Program/Script/Editor.js
@@ -1,6 +1,13 @@
 class FieldEdit {
     constructor() {
-        this.player = 0
+        this.player = new Player()
         this.tileMap = new TileMap()
+
+        this.canvas = document.createElement('canvas')
+        this.ctx = canvas.getContext('2d')
+    }
+
+    handleTick() {
+
     }
 }
--- Program/Script/Player.js
+++ Program/Script/Player.js
@@ -1,3 +1,13 @@
 class Player {
+    constructor() {
+        this.rect = new Rect2D(0, 0, 40, 40)
+    }
 
+    handleTick() {
+        render()
+    }
+
+    render() {
+
+    }
 }
