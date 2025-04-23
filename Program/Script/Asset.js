976218cb5f017e22de9fd4063e3fa15776ee4cb1
pupungeggang6 <pupungeggang6@gmail.com>
2025-04-24 02:10:30 +0900

    Added New Image

--- Program/Script/Asset.js
+++ Program/Script/Asset.js
@@ -1,7 +1,31 @@
 let img = {
-    
+    button: {
+        newFile: new Image(),
+        saveFile: new Image(),
+        openFile: new Image(),
+
+        tile: new Image(),
+        thing: new Image(),
+        start: new Image(),
+        goal: new Image(),
+
+        play: new Image(),
+        stop: new Image(),
+        pause: new Image(),
+    }
 }
 
 function imageLoad() {
+    img.button.newFile.src = 'Image/Button/ButtonNew.png'
+    img.button.openFile.src = 'Image/Button/ButtonOpen.png'
+    img.button.saveFile.src = 'Image/Button/ButtonSave.png'
 
+    img.button.tile.src = 'Image/Button/ButtonTile.png'
+    img.button.thing.src = 'Image/Button/ButtonThing.png'
+    img.button.start.src = 'Image/Button/ButtonStart.png'
+    img.button.goal.src = 'Image/Button/ButtonGoal.png'
+
+    img.button.play.src = 'Image/Button/ButtonPlay.png'
+    img.button.stop.src = 'Image/Button/ButtonStop.png'
+    img.button.pause.src = 'Image/Button/ButtonPause.png'
 }
